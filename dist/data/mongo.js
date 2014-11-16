7e343706b7b2d30c2b226194152f489ed7d6be14
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2014-11-16 10:54:22 +0000

    Initial initial script for MongoDB, adding testclient to DB

---
+++ dist/data/mongo.js
@@ -0,0 +1,8 @@
+db.oauth_clients.insert(
+        {
+            "client_id": "testclient", 
+            "client_secret": "$2y$14$f3qml4G2hG6sxM26VMq.geDYbsS089IBtVJ7DlD05BoViS9PFykE2", 
+            "redirect_uri":  "/oauth/receivecode", 
+            "grant_types":   null
+        }
+)
